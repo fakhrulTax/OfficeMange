d5f087a937180475a15e4067ef3e78af8c600bc6
fakhrul <fakhrultax@gmail.com>
2024-01-31 23:26:43 +0600

    Appeal Register Done

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -42,6 +42,7 @@ class AppealController extends Controller
             'assessment_year' => 'required|digits:8',
             'main_income' => 'required|numeric',
             'main_tax' => 'required|numeric',
+            'tax_type' => 'required',
             'revise_income' => 'required|numeric',
             'revise_tax' => 'required|numeric',
         ]);
@@ -54,10 +55,12 @@ class AppealController extends Controller
         }
 
         //Update Arrear
-        if($request->arrear_type == 'tax')
+        if($request->tax_type == 'tax')
         {
             Arrear::updateArrearStatusAmountOrFine($request->tin, $request->assessment_year, 'undisputed', $request->revise_tax, false);
-        }else
+        }
+        
+        if($request->tax_type == 'fine')
         {
             Arrear::updateArrearStatusAmountOrFine($request->tin, $request->assessment_year, 'undisputed', false , $request->revise_tax);
         }
@@ -74,6 +77,7 @@ class AppealController extends Controller
          $appeal->assessment_year = $request->assessment_year;
          $appeal->main_income = $request->main_income;
          $appeal->main_tax = $request->main_tax;
+         $appeal->tax_type = $request->tax_type;
          $appeal->revise_income = $request->revise_income;
          $appeal->revise_tax = $request->revise_tax;
          $appeal->circle = Auth::user()->circle;
@@ -88,13 +92,67 @@ class AppealController extends Controller
     //Collection Edit
     public function edit($id)
     {
-        return 'go';
+        $appeal = Appeal::findOrFail($id);
+        return view('circle.appeal.edit',[
+            'title' => 'Appeal | Update Appeal',
+            'appeal' => $appeal
+        ]);
     }
 
     //Collection Update
-    public function update(Request $request){
+    public function update(Request $request, $id){
         
-        return 'go';   
+        // Validation rules
+        $request->validate([
+            'type' => 'required',
+            'tin' => 'required|digits:12',
+            'appeal_order' => 'required|string',
+            'appeal_order_date' => 'required|date_format:d-m-Y',
+            'appeal_disposal_date' => 'required|date_format:d-m-Y',
+            'assessment_year' => 'required|digits:8',
+            'main_income' => 'required|numeric',
+            'main_tax' => 'required|numeric',            
+            'revise_income' => 'required|numeric',
+            'revise_tax' => 'required|numeric',
+        ]);
+
+        //Check Arrear
+        $arrear = Arrear::checkArrear($request->tin, $request->assessment_year);
+        if (!$arrear) {
+            Toastr::error('There is no arrear for this TIN and Assessment Year', 'danger');
+            return back()->withInput();
+        }
+
+        $appeal = Appeal::findOrFail($id);
+
+        //Update Arrear
+        if($appeal->tax_type == 'tax')
+        {
+            Arrear::updateArrearStatusAmountOrFine($request->tin, $request->assessment_year, 'undisputed', $request->revise_tax, false);
+        }
+        
+        if($appeal ->tax_type == 'fine')
+        {
+            Arrear::updateArrearStatusAmountOrFine($request->tin, $request->assessment_year, 'undisputed', false , $request->revise_tax);
+        }
+        
+         // Assign values from the form to the model
+         $appeal->type = $request->type;
+         $appeal->tin = $request->tin;
+         $appeal->appeal_order = $request->appeal_order;
+         $appeal->appeal_order_date = date('Y-m-d', strtotime($request->appeal_order_date));
+         $appeal->appeal_disposal_date = date('Y-m-d', strtotime($request->appeal_disposal_date));
+         $appeal->assessment_year = $request->assessment_year;
+         $appeal->main_income = $request->main_income;
+         $appeal->main_tax = $request->main_tax;
+         $appeal->revise_income = $request->revise_income;
+         $appeal->revise_tax = $request->revise_tax;
+         $appeal->circle = Auth::user()->circle;
+ 
+         // Save the model to the database
+         $appeal->save();   
+         Toastr::success( ucwords($request->type) . ' Appeal Update Successful', 'success');
+        return redirect()->route('circle.appeal.index');  
     }
 
     //Search
