f0d0f230d5d0e46590165a72a9eaf0d17daa9950
creativesaiful <saifulislamsapon@gmail.com>
2024-02-03 17:24:18 +0600

    fabicon, menu active showing and some minor update

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,42 +1,42 @@
 @extends('app')
 
-@section('title', 'User Profile');
+@section('title', 'Profile')
 
 @section('content')
 
-
-    <section class="content">
+<section class="content">
       
 
-            <div class="card">
-                <div class="card-body">
-                    <h5>Name: {{ $user->name }}</h5>
-    
-                    <h5>Designation: {{ $user->designation }}</h5>
-    
-    
-                    <h5>User Type: {{ $user->user_role }}</h5>
-    
-    
-                    @if ($user->range)
-                        <h5>Range: {{ $user->range }}</h5>
-                    @endif
-    
-                    @if ($user->circle)
-                        <h5>Circle: {{ $user->circle }}</h5>
-                    @endif
-    
-                    <h5>Phone: {{ $user->mobile_number }}</h5>
-    
-                    <h5 class="">Email: {{ $user->email }}</h5>
-    
-                    <h5 class="">Office Name: {{ $user->office_name }}</h5>
-    
-                </div>
-            </div>
-      
-    </section>
+    <div class="card">
+        <div class="card-body">
+            <h5>Name: {{ $user->name }}</h5>
 
-    
+            <h5>Designation: {{ $user->designation }}</h5>
+
+
+            <h5>User Type: {{ $user->user_role }}</h5>
+
+
+            @if ($user->range)
+                <h5>Range: {{ $user->range }}</h5>
+            @endif
+
+            @if ($user->circle)
+                <h5>Circle: {{ $user->circle }}</h5>
+            @endif
+
+            <h5>Phone: {{ $user->mobile_number }}</h5>
+
+            <h5 class="">Email: {{ $user->email }}</h5>
+
+            <h5 class="">Office Name: {{ $user->office_name }}</h5>
+
+        </div>
+    </div>
+
+</section>
+
+
+
 
 @endsection
