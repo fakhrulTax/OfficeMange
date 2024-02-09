27abfb7db6c7b71d72df0fe37b94008406f89338
fakhrul <fakhrultax@gmail.com>
2024-02-09 16:27:47 +0600

    advance table migration

---
+++ database/migrations/2024_02_09_101854_create_advances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tin');
+            $table->integer('advance_assessment_year')->length(8);
+            $table->integer('return_submitted_assessment_year')->length(8);
+            $table->BigInteger('income');
+            $table->BigInteger('tax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advances');
+    }
+};
